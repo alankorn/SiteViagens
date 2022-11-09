11141a8a227ed8b91b99c2b9fad619a9be9a7385
alankorn <alanferreira1080@gmail.com>
2022-11-08 22:50:59 -0300

    Criação de duas telas e tentativa do carrinho

---
+++ app.js
@@ -0,0 +1,51 @@
+const destinoFortaleza = {
+    valor_pacote: '1500',
+    qtd_dias: '5',
+    classificacao_hotel: '4',
+    companhia_aerea: 'Gol',
+    qtd_pessoas:'1',
+  };
+
+  const destinoBelem = {
+    valor_pacote: '1200',
+    qtd_dias: '5',
+    classificacao_hotel: '4',
+    companhia_aerea: 'Gol',
+    qtd_pessoas:'1',
+  };
+
+  const destinoCuiaba = {
+    valor_pacote: '1000',
+    qtd_dias: '5',
+    classificacao_hotel: '4',
+    companhia_aerea: 'Gol',
+    qtd_pessoas:'1',
+  };
+  const destinoRioJaneiro = {
+    valor_pacote: '1500',
+    qtd_dias: '5',
+    classificacao_hotel: '4',
+    companhia_aerea: 'Gol',
+    qtd_pessoas:'1',
+  };
+  const destinoFlorianopolis = {
+    valor_pacote: '900',
+    qtd_dias: '5',
+    classificacao_hotel: '4',
+    companhia_aerea: 'Gol',
+    qtd_pessoas:'1',
+  };
+
+  let carrinho = [];
+
+  function addProd(destino){
+      carrinho.push(destino)
+      console.log("Adicionado com sucesso")
+  }
+
+  var a = ""
+  var a = document.getElementById("Florianopolis").value
+
+
+  
+  
