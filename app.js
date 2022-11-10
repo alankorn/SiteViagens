0a801e2927ce710a3e0f5d6ce6fb95328a07812c
alankorn <alanferreira1080@gmail.com>
2022-11-10 19:14:22 -0300

    correção do diretório

--- app.js
+++ app.js
@@ -1,51 +1,68 @@
-const destinoFortaleza = {
-    valor_pacote: '1500',
-    qtd_dias: '5',
-    classificacao_hotel: '4',
-    companhia_aerea: 'Gol',
-    qtd_pessoas:'1',
-  };
 
-  const destinoBelem = {
-    valor_pacote: '1200',
-    qtd_dias: '5',
-    classificacao_hotel: '4',
-    companhia_aerea: 'Gol',
-    qtd_pessoas:'1',
-  };
-
-  const destinoCuiaba = {
-    valor_pacote: '1000',
-    qtd_dias: '5',
-    classificacao_hotel: '4',
-    companhia_aerea: 'Gol',
-    qtd_pessoas:'1',
-  };
-  const destinoRioJaneiro = {
-    valor_pacote: '1500',
-    qtd_dias: '5',
-    classificacao_hotel: '4',
-    companhia_aerea: 'Gol',
-    qtd_pessoas:'1',
-  };
-  const destinoFlorianopolis = {
-    valor_pacote: '900',
-    qtd_dias: '5',
-    classificacao_hotel: '4',
-    companhia_aerea: 'Gol',
-    qtd_pessoas:'1',
-  };
-
-  let carrinho = [];
-
-  function addProd(destino){
-      carrinho.push(destino)
-      console.log("Adicionado com sucesso")
-  }
-
-  var a = ""
-  var a = document.getElementById("Florianopolis").value
-
-
+    
+    
+    
+    
+    const itens = [
+      {
+      id: 0,
+      nome: 'DestinoFortaleza',
+      valor_pacote: '1500',
+      qtd_dias: '5',
+      classificacao_hotel: '4',
+      companhia_aerea: 'Gol',
+      qtd_pessoas:'1',
+      quantidade: 0
+      },
+      {
+      id: 1,
+      nome: 'DestinoBelem',
+      valor_pacote: '1200',
+      qtd_dias: '5',
+      classificacao_hotel: '4',
+      companhia_aerea: 'Gol',
+      qtd_pessoas:'1',
+      quantidade: 0
+    },
+    {
+      id: 2,
+      nome: 'DestinoCuiaba',
+      valorpacote: '1000',
+      qtd_dias: '5',
+      classificacao_hotel: '4',
+      companhia_aerea: 'Gol',
+      qtd_pessoas:'1',
+      quantidade: 0
+    },
+    {
+      id: 3,
+      nome: 'DestinoRiodeJaneiro',
+      valor_pacote: '1200',
+      qtd_dias: '5',
+      classificacao_hotel: '4',
+      companhia_aerea: 'Gol',
+      qtd_pessoas:'1',
+      quantidade: 0
+    },
+    {
+      id: 3,
+      nome: 'DestinoFlorianopolis',
+      valor_pacote: '900',
+      qtd_dias: '5',
+      classificacao_hotel: '4',
+      companhia_aerea: 'Gol',
+      qtd_pessoas:'1',
+      qauntidade: 0
+    },
+  ]
   
+  inicializarLoja = () => {
+      var containerProdutos = document.querySelector("#destinoslista");
+      itens.map((val)=>{
+      
+      })
+  
+    }
+    
+    inicializarLoja();
   
